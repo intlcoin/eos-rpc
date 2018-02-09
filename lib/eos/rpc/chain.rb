20c2c322e0b83be6b33da178b24dcb15699e6e91
Yanis Tsaunya <yanis88@gmail.com>
2018-02-09 10:59:23 +0300

    added more chain methods

--- lib/eos/rpc/chain.rb
+++ lib/eos/rpc/chain.rb
@@ -18,6 +18,24 @@ module Eos
         json = { name: name.to_s }.to_json
         self.class.post('/get_account', body: json).parsed_response
       end
+
+      def get_code(account_name)
+        json = { account_name: account_name.to_s }.to_json
+        self.class.post('/get_code', body: json).parsed_response
+      end
+
+      def get_table_rows(scope:, code:, table:, lower_bound: 0, upper_bound: -1, limit: 10)
+        json = {
+          scope: scope.to_s,
+          code: code.to_s,
+          table: table.to_s,
+          json: true,
+          lower_bound: upper_bound,
+          upper_bound: lower_bound,
+          limit: limit
+        }.to_json
+        self.class.post('/get_table_rows', body: json).parsed_response
+      end
     end
   end
 end
