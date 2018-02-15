09f4c316ed0bf7c9703981608b2ef42309c4d9f1
Yanis Tsaunya <yanis88@gmail.com>
2018-02-15 23:38:51 +0300

    more tests

--- test/chain_test.rb
+++ test/chain_test.rb
@@ -12,4 +12,8 @@ describe Eos::Rpc::Chain, :vcr do
   it "gets block by number or id" do
     @chain.get_block(5).must_be_instance_of Hash
   end
+
+  it "it gets code by account name" do
+    @chain.get_block('inita').must_be_instance_of Hash
+  end
 end
--- test/eos_rpc_test.rb
+++ test/eos_rpc_test.rb
@@ -4,8 +4,4 @@ class EosRpcTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Eos::Rpc::VERSION
   end
-
-  def test_it_does_something_useful
-    assert true
-  end
 end
